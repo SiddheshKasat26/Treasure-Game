02cfa0fe9bb911c6cc72e55b7175760f35e59f1d
SiddheshKasat26 <142038176+SiddheshKasat26@users.noreply.github.com>
2023-08-14 15:37:19 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Treasure-Game
+## Find Treasure inside CASTLE
+
+This is "Find Treasure inside CASTLE" game. Here, I used conditionals such as `if`, `else`, and `elif` statements to lay out the logic and the story's path in the program. 
+ 
+Also I used `input` statement to take inputs from users also I used `f-string` to mention Player name inside some of the Commands.
+
+This Game is developed by using python only.
+
+### Story Line of the GAME
+
+* "Does user wanna play the game? Y or N."
+* "If N, then user should come again and try this game.This game is totally full of fun."
+* "Now, if user choose to play game then he/she start from the main door."
+* "You have 4 Options means 4 rooms from which user can choose whichever he/she wants."
+* "In Room1 there is hungry LION who will it the player definitely."
+* "Tn Room2 there are again 2 rooms inside it i.e. Left and Right."
+* "But Both rooms doesn't contain TREASURE so it is game over for user."
+* "If user chooses Room3 then also the game is over for user because that room is full of snakes instead of treasure."
+* "Now Only remaining option is Room4 so definitely the treasure is here. But Wait a Minute.....Is it really that's simple?"
+* "Inside Room4 there are again 2 rooms(Left one and Right one). Left room is full of dynamites while Right room is full of TREASURE."
+* "If user chooses right room, then he/she will win all the treasure and successfully complete the game."
---
+++ main.py
@@ -0,0 +1,55 @@
+print('''
+                                           o         _ _ _ _ _
+                                                _----|         I-I-I-I-I
+                             _ _ _ _ _ _      o  ----|     o   \~~`~'~~/
+                             ]-I-I-I-I-[ _----|      |_----|    |.    |
+                             \~`\~~~/'~/  ----|     / \----|    |  /^\|
+                              [*] ' __|       ^    / ^ \   ^    |_ |*||
+                              |__    ,|      / \  /    `\ / \   |  ===|
+                            __|  ___ ,|__   /    /=_=_=_=\   \  |,  __|
+                            I_I__I_I__I_I  (====(_________)___|_|_____|___
+                            \-\--|-|--/-/  |'    I~~[ ]~~I I_I__|IIII|_I_l
+                             | [ ]    '|   | [~] |_`_~_ _[  \-\--|-|--/-/
+                             |.   | |' |___|____`I_I_|_I_I___|---------|
+                            / \| [] ~ .|_|-|_|-|-|_|-|_|-|_|-| []   [] |
+                           <===>  |  ~.|-=-=-=-=-=-=-=-=-=-=-|~  |  ~ / \
+                           | []|`   []~||.|.|.|.|.|.|.|.|.|.||-~   ~ <===>
+   O O      o o            | []| ` |   |/////////I\\\\\\\\\\||__. ~| |[*]I
+  O * O    o * o           <===> ~   ' ||||| |   |   | ||||.||  []  ~<===>
+   O O      o o             \T/  |~|-- ||||| | O | O | ||||.|| . |'~  \T/
+   \I        I/              |     ~.~_||||| |~~~|~~~| ||||.|| | ~   | |
+____I/______\I____\/..___.../|' v . | .|||||/____|____\|||| /|. . | . .|\.\/_
+''')
+Player = input("What is your name? ")
+print("Welcome to the CASTLE.")
+print("Your mission is to find the treasure which is hidden inside the Castle.")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+ans = input("Are you ready? Y or N. ")
+if ans == 'Y':
+    print(f"All The Best {Player}👍.We hope you find the TREASURE 🤞.")
+    print("So, Let's Begin💪 -->")
+    print("You are at Main Door, and in front of you there are 5 rooms.(Room names are 1,2,3 and 4.)")
+    room_number = int(input(f"Which room do you want to go {Player}? 1 2 3 4 => "))
+    if room_number == 1:
+        print("Sorry, You Died by falling in hole which was full of spikes😔.\nBETTER LUCK NEXT TIME👍")
+    elif room_number == 2:
+        room2 = input("Nice Choice! Now, which room you want to choose? Left or Right.(Type Left or Right only!) ")
+        if room2 == "Left":
+            print("Sorry, You Died here because LION ate you🦁.\nBETTER LUCK NEXT TIME👍")
+        else:
+            print("Sorry, You Died here because this room is FULL of Electricity(1000 Volts)🔌⚡💥😵.\nBETTER LUCK NEXT TIME👍")
+    elif room_number == 3:
+        print("Sorry, You Died in the room where there are more than 100 poisonous snakes🐍.\nBETTER LUCK NEXT TIME👍")
+    elif room_number == 4:
+        room4 = input("Nice Choice! Now, which room you want to choose? Left or Right.(Type Left or Right only!) ")
+        if room4 == "Left":
+            print("Sorry, You Died here because this room is full of DYNAMITES🧨.\nBETTER LUCK NEXT TIME👍")
+        else:
+            print(f"HURRAY!,You Did it {Player}🎉🎉🎉🎉🎉🎉.The room of Treasure is now yours💰💰💰💰💰💰.")
+elif ans == 'N':
+    print("Sorry to see you go, Please visit again.\nThis game is FUN!!!😀")
+else :
+    print("Ohh! You entered a wrong key. Try again!")
